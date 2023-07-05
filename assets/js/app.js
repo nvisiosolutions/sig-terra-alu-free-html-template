b9ed98ac8ef30a2d6da62fad4152f075609ce17a
asmsaiff <info@saifullah.co>
2023-07-05 11:40:58 +0600

    initial project

---
+++ assets/js/app.js
@@ -0,0 +1,54 @@
+const slides = document.querySelectorAll(".page");
+const allPage = document.getElementById('all-page')
+const currentPage = document.getElementById('current-page')
+// current slide counter
+let curSlide = 0;
+// maximum number of slides
+let maxSlide = slides.length - 1;
+// select next slide button
+const nextSlide = document.querySelector("#next");
+// select prev slide button
+const prevSlide = document.querySelector("#prev");
+
+// Set all page number
+allPage.innerHTML = slides.length
+// Set current page number
+currentPage.innerHTML = curSlide + 1
+
+slides.forEach((slide, indx) => {
+    slide.style.transform = `translateY(${100 * (indx - curSlide)}%)`;
+});
+
+// add event listener and next slide functionality
+nextSlide.addEventListener("click", function () {
+    if (curSlide === maxSlide) {
+        curSlide = 0;
+    } else {
+        curSlide++;
+    }
+
+    currentPage.innerHTML = curSlide + 1
+
+    slides.forEach((slide, indx) => {
+        slide.style.transform = `translateY(${100 * (indx - curSlide)}%)`;
+        slide.style.transition = `all 0.5s`;
+    });
+});
+
+// add event listener and navigation functionality
+prevSlide.addEventListener("click", function () {
+    // check if current slide is the first and reset current slide to last
+    if (curSlide === 0) {
+        curSlide = maxSlide;
+    } else {
+        curSlide--;
+    }
+
+    currentPage.innerHTML = curSlide + 1
+  
+    //   move slide by 100%
+    slides.forEach((slide, indx) => {
+        slide.style.transform = `translateY(${100 * (indx - curSlide)}%)`;
+        slide.style.transition = `all 0.5s`;
+    });
+});
