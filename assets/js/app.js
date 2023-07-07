3ba5dbcd8cfc3e3c8d3ec02b5db05c196217b67a
asmsaiff <info@saifullah.co>
2023-07-07 12:02:27 +0600

    prevent design

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,100 +1,100 @@
-const currentPage = document.getElementById('current-page')
-const allPage = document.getElementById('all-page')
+// const currentPage = document.getElementById('current-page')
+// const allPage = document.getElementById('all-page')
 
-let currentSectionIndex = 0;
-const sections = document.getElementsByClassName('page');
+// let currentSectionIndex = 0;
+// const sections = document.getElementsByClassName('page');
 
-const prevButton = document.getElementById('prev');
-const nextButton = document.getElementById('next');
-let touchStartY;
+// const prevButton = document.getElementById('prev');
+// const nextButton = document.getElementById('next');
+// let touchStartY;
 
-// Initialize Current page and total page
-const currentSection = currentSectionIndex + 1;
-const totalSections = sections.length;
+// // Initialize Current page and total page
+// const currentSection = currentSectionIndex + 1;
+// const totalSections = sections.length;
 
-currentPage.innerHTML = currentSection
-allPage.innerHTML = totalSections
+// currentPage.innerHTML = currentSection
+// allPage.innerHTML = totalSections
 
-prevButton.disabled = true
-nextButton.disabled = false
+// prevButton.disabled = true
+// nextButton.disabled = false
 
-function showSection(index) {
-    if (index >= 0 && index < sections.length) {
-        const currentSection = sections[currentSectionIndex];
-        const nextSection = sections[index];
+// function showSection(index) {
+//     if (index >= 0 && index < sections.length) {
+//         const currentSection = sections[currentSectionIndex];
+//         const nextSection = sections[index];
 
-        if (index > currentSectionIndex) {
-            // Show next section
-            currentSection.style.transform = `translateY(-100%)`;
-            nextSection.style.transform = `translateY(0)`;
-        } else {
-            // Show previous section
-            currentSection.style.transform = `translateY(100%)`;
-            nextSection.style.transform = `translateY(0)`;
-        }
+//         if (index > currentSectionIndex) {
+//             // Show next section
+//             currentSection.style.transform = `translateY(-100%)`;
+//             nextSection.style.transform = `translateY(0)`;
+//         } else {
+//             // Show previous section
+//             currentSection.style.transform = `translateY(100%)`;
+//             nextSection.style.transform = `translateY(0)`;
+//         }
 
-        currentSectionIndex = index;
-        updateSectionInfo()
-    }
-}
+//         currentSectionIndex = index;
+//         updateSectionInfo()
+//     }
+// }
 
-function showNext() {
-    showSection(currentSectionIndex + 1);
-    handleDisabledButton()
-}
+// function showNext() {
+//     showSection(currentSectionIndex + 1);
+//     handleDisabledButton()
+// }
 
-function showPrevious() {
-    showSection(currentSectionIndex - 1);
-    handleDisabledButton()
-}
+// function showPrevious() {
+//     showSection(currentSectionIndex - 1);
+//     handleDisabledButton()
+// }
 
-function handleTouchStart(event) {
-    touchStartY = event.touches[0].clientY;
-  }
+// function handleTouchStart(event) {
+//     touchStartY = event.touches[0].clientY;
+//   }
 
-function handleTouchMove(event) {
-    const touchEndY = event.changedTouches[0].clientY
-    const touchDiff = touchEndY - touchStartY;
-    if (touchDiff > 0 && touchDiff > 100) {
-        showPrevious();
-    } else if (touchDiff < 0 && Math.abs(touchDiff) > 100) {
-        showNext();
-    }
-}
+// function handleTouchMove(event) {
+//     const touchEndY = event.changedTouches[0].clientY
+//     const touchDiff = touchEndY - touchStartY;
+//     if (touchDiff > 0 && touchDiff > 100) {
+//         showPrevious();
+//     } else if (touchDiff < 0 && Math.abs(touchDiff) > 100) {
+//         showNext();
+//     }
+// }
 
-function updateSectionInfo() {
-    const currentSection = currentSectionIndex + 1;
-    const totalSections = sections.length;
+// function updateSectionInfo() {
+//     const currentSection = currentSectionIndex + 1;
+//     const totalSections = sections.length;
 
-    currentPage.innerHTML = currentSection
-    allPage.innerHTML = totalSections
-}
+//     currentPage.innerHTML = currentSection
+//     allPage.innerHTML = totalSections
+// }
 
-function handleScroll(event) {
-    const delta = Math.sign(event.deltaY);
+// function handleScroll(event) {
+//     const delta = Math.sign(event.deltaY);
 
-    if (delta > 0) {
-        showNext();
-    } else if (delta < 0) {
-        showPrevious();
-    }
+//     if (delta > 0) {
+//         showNext();
+//     } else if (delta < 0) {
+//         showPrevious();
+//     }
 
-    handleDisabledButton()
-}
+//     handleDisabledButton()
+// }
 
-function handleDisabledButton() {
-    if (currentSectionIndex === 0) {
-        prevButton.disabled = true
-        nextButton.disabled = false
-    } else if (currentSectionIndex === sections.length - 1) {
-        prevButton.disabled = false
-        nextButton.disabled = true
-    } else {
-        prevButton.disabled = false
-        nextButton.disabled = false
-    }
-}
+// function handleDisabledButton() {
+//     if (currentSectionIndex === 0) {
+//         prevButton.disabled = true
+//         nextButton.disabled = false
+//     } else if (currentSectionIndex === sections.length - 1) {
+//         prevButton.disabled = false
+//         nextButton.disabled = true
+//     } else {
+//         prevButton.disabled = false
+//         nextButton.disabled = false
+//     }
+// }
 
-window.addEventListener('touchstart', handleTouchStart);
-window.addEventListener('touchend', handleTouchMove);
-window.addEventListener('wheel', handleScroll);
+// // window.addEventListener('touchstart', handleTouchStart);
+// // window.addEventListener('touchend', handleTouchMove);
+// // window.addEventListener('wheel', handleScroll);
